157f2a4212bc5e6d5d0762f3c9d376eed7ed0214
mtat27 <mtat_27@hotmail.com>
2024-06-12 22:50:23 -0600

    made bookbot give a final report

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ def main():
     words = count_words(text)
     print (f"your file has {words} words in it")
     letters = count_letters(text)
-    print (letters)
+    letter_report(letters)
+    print ("--- end of report ---")
 
 def read_book(book_path):
     with open(book_path) as f:
@@ -23,5 +24,18 @@ def count_letters(text):
         else:
             letters[lowered] = 1
     return letters
+
+def sort_on(dict):
+    return dict["num"]
+
+def letter_report(letters):
+    list_letters = []
+    for char in letters:
+        if char.isalpha():
+            new_dict = {"char": char, "num": letters[char]}
+            list_letters.append(new_dict)
+    list_letters.sort(reverse=True, key=sort_on)
+    for item in list_letters:
+        print (f"there are {item['num']} instances of the letter {item['char']}")
 
 main()
