e7a11966eb539535c602b6bfba850de930ac4f9f
mtat27 <mtat_27@hotmail.com>
2024-06-12 21:47:51 -0600

    added word counting functionality to bookbot

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = read_book(book_path)
-    print (text)
+    words = count_words(text)
+    print (words)
 
 def read_book(book_path):
     with open(book_path) as f:
         return f.read()
+
+def count_words(text):
+    total_words = text.split()
+    return len(total_words)
 
 main()
