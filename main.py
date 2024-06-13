8a3b0e25f8b35f7ebd906457afad14418a39feff
mtat27 <mtat_27@hotmail.com>
2024-06-12 22:05:20 -0600

    added ability for bookbot to count letters

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     text = read_book(book_path)
     words = count_words(text)
     print (f"your file has {words} words in it")
+    letters = count_letters(text)
+    print (letters)
 
 def read_book(book_path):
     with open(book_path) as f:
@@ -11,5 +13,15 @@ def read_book(book_path):
 def count_words(text):
     total_words = text.split()
     return len(total_words)
+
+def count_letters(text):
+    letters = {}
+    for char in text:
+        lowered = char.lower()
+        if lowered in letters:
+            letters[lowered] += 1 
+        else:
+            letters[lowered] = 1
+    return letters
 
 main()
